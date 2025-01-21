26e2c8887b0bdfab3fd69e35ff21ee4c26793ce5
yagovysk <yago09regis@gmail.com>
2025-01-21 14:18:07 -0300

    Add to Cart' button functionality Implemented cart functionality, allowing users to add products

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,7 @@
 class LineItemsController < ApplicationController
-  before_action :set_line_item, only: %i[ show edit update destroy ]
+  include CurrentCart
+  before_action :set_cart, only: %i[create]
+  before_action :set_line_item, only: %i[show edit update destroy]
 
   # GET /line_items or /line_items.json
   def index
@@ -8,7 +10,7 @@ class LineItemsController < ApplicationController
 
   # GET /line_items/1 or /line_items/1.json
   def show
-    puts "Este item é do produto #{@line_item.product.title}" # Adicionado aqui
+    Rails.logger.info "Este item é do produto #{@line_item.product.title}" # Para logs de depuração
   end
 
   # GET /line_items/new
@@ -22,11 +24,16 @@ class LineItemsController < ApplicationController
 
   # POST /line_items or /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+    product = Product.find_by(id: params[:product_id])
+    unless product
+      redirect_to store_index_url, alert: "Produto não encontrado." and return
+    end
+
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: "Line item was successfully created." }
+        format.html { redirect_to cart_url(@line_item.cart), notice: "Item adicionado ao carrinho com sucesso." }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +46,7 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: "Line item was successfully updated." }
+        format.html { redirect_to line_item_url(@line_item), notice: "Item atualizado com sucesso." }
         format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -51,21 +58,24 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1 or /line_items/1.json
   def destroy
     @line_item.destroy
-
     respond_to do |format|
-      format.html { redirect_to line_items_path, status: :see_other, notice: "Line item was successfully destroyed." }
+      format.html { redirect_to cart_url(@line_item.cart), notice: "Item removido do carrinho com sucesso." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+  # Use callbacks to compartilhar configurações comuns ou restrições entre ações.
+  def set_line_item
+    @line_item = LineItem.find_by(id: params[:id])
+    unless @line_item
+      redirect_to store_index_url, alert: "Item não encontrado."
     end
+  end
+
+  # Apenas permitir uma lista de parâmetros confiáveis.
+  def line_item_params
+    params.require(:line_item).permit(:product_id, :cart_id)
+  end
 end
