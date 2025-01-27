31339985ab16a452d0533f90a20d5f3d7263c1c4
yagovysk <yago09regis@gmail.com>
2025-01-27 11:04:06 -0300

    Added dynamic fields and payment validations to the form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,83 +1,86 @@
-include CurrentCart
-before_action :set_cart, only: %i[new create]
-before_action :ensure_cart_isnt_empty, only: %i[new]
-before_action :set_order, only: %i[show edit update destroy]
+class OrdersController < ApplicationController
+  include CurrentCart
 
-# GET /orders or /orders.json
-def index
-  @orders = Order.all
-end
+  before_action :set_cart, only: %i[new create]
+  before_action :ensure_cart_isnt_empty, only: %i[new]
+  before_action :set_order, only: %i[show edit update destroy]
 
-# GET /orders/1 or /orders/1.json
-def show
-end
+  # GET /orders or /orders.json
+  def index
+    @orders = Order.all
+  end
 
-# GET /orders/new
-def new
-  @order = Order.new
-end
+  # GET /orders/1 or /orders/1.json
+  def show
+  end
 
-# GET /orders/1/edit
-def edit
-end
+  # GET /orders/new
+  def new
+    @order = Order.new
+  end
 
-# POST /orders or /orders.json
-def create
-  @order = Order.new(order_params)
-  @order.add_line_items_from_cart(@cart)
+  # GET /orders/1/edit
+  def edit
+  end
 
-  respond_to do |format|
-    if @order.save
-      Cart.destroy(session[:cart_id])
-      session[:cart_id] = nil
+  # POST /orders or /orders.json
+  def create
+    @order = Order.new(order_params)
+    @order.add_line_items_from_cart(@cart)
 
-      format.html { redirect_to store_index_url, notice: 'Thank you for your order.' }
-      format.json { render :show, status: :created, location: @order }
-    else
-      format.html { render :new, status: :unprocessable_entity }
-      format.json { render json: @order.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @order.save
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+
+        format.html { redirect_to store_index_url, notice: 'Thank you for your order.' }
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
     end
   end
-end
 
-# PATCH/PUT /orders/1 or /orders/1.json
-def update
-  respond_to do |format|
-    if @order.update(order_params)
-      format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-      format.json { render :show, status: :ok, location: @order }
-    else
-      format.html { render :edit, status: :unprocessable_entity }
-      format.json { render json: @order.errors, status: :unprocessable_entity }
+  # PATCH/PUT /orders/1 or /orders/1.json
+  def update
+    respond_to do |format|
+      if @order.update(order_params)
+        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+        format.json { render :show, status: :ok, location: @order }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
     end
   end
-end
 
-# DELETE /orders/1 or /orders/1.json
-def destroy
-  @order.destroy
+  # DELETE /orders/1 or /orders/1.json
+  def destroy
+    @order.destroy
 
-  respond_to do |format|
-    format.html { redirect_to orders_path, status: :see_other, notice: 'Order was successfully destroyed.' }
-    format.json { head :no_content }
+    respond_to do |format|
+      format.html { redirect_to orders_path, status: :see_other, notice: 'Order was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
-end
 
-private
+  private
 
-# Use callbacks to share common setup or constraints between actions.
-def set_order
-  @order = Order.find(params[:id])
-end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
 
-# Only allow a list of trusted parameters through.
-def order_params
-  params.require(:order).permit(:name, :address, :email, :pay_type)
-end
+  # Only allow a list of trusted parameters through.
+  def order_params
+    params.require(:order).permit(:name, :address, :email, :pay_type)
+  end
 
-# Ensure the cart isn't empty before creating an order.
-def ensure_cart_isnt_empty
-  if @cart.line_items.empty?
-    redirect_to store_index_url, notice: 'Your cart is empty'
+  # Ensure the cart isn't empty before creating an order.
+  def ensure_cart_isnt_empty
+    if @cart.line_items.empty?
+      redirect_to store_index_url, notice: 'Your cart is empty'
+    end
   end
 end
