7cb80cf87a23b0fcd84a1c40cf792b3b2274d522
yagovysk <yago09regis@gmail.com>
2025-02-06 20:45:13 -0300

    Implementing locale selection for the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
-
+  
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
@@ -11,12 +11,14 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do  # Adicionando o escopo opcional do idioma
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
   end
 end
